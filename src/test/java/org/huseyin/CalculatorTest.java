28f78bb5ead64c55fa0f8a76548d2d868ff218d2
Huseyin M Ozyilmaz <huseyinozyilmaz@users.noreply.github.com>
2017-09-14 18:03:16 +0100

    Added extra unit test to cover print method

--- src/test/java/org/huseyin/CalculatorTest.java
+++ src/test/java/org/huseyin/CalculatorTest.java
@@ -15,4 +15,10 @@ public class CalculatorTest {
         assertEquals("1 + 2 must be 3", 3, calc.add(1, 2));
         assertEquals("1 + 0 must be 1", 1, calc.add(1, 0));
     }
+
+    @Test(expected = Test.None.class /* no exception expected */)
+    public void canCallPrint() {
+        Calculator calc = new Calculator();
+        calc.print();
+    }
 }
