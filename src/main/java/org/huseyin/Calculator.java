edccb77c57a40855c5c8d26ce8136a4b26ecd28e
Huseyin M Ozyilmaz <huseyinozyilmaz@users.noreply.github.com>
2017-09-14 17:54:17 +0100

    Introduced a vulnerability to test Sonar Quality Gate

--- src/main/java/org/huseyin/Calculator.java
+++ src/main/java/org/huseyin/Calculator.java
@@ -8,4 +8,13 @@ public class Calculator {
     public int add (int x, int y) {
         return x+y;
     }
+
+    public void print() {
+        try {
+            System.out.println("Calculator");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
 }
